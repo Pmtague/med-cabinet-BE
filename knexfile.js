c84492d1d2510a6f0be02d0d371f1c2c4da3ca0c
vladmog <vladmog@gmail.com>
2019-08-27 16:58:02 -0700

    switched back to users db

--- knexfile.js
+++ knexfile.js
@@ -2,30 +2,11 @@
 
 module.exports = {
 
-  development: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: './data/cannabis_strains2.db'
-    },
-    migrations: {
-      directory: './data/migrations'
-    },
-    seeds: {
-      directory: './data/seeds'
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys=ON', done);
-      }
-    }
-  },
-
   // development: {
   //   client: 'sqlite3',
   //   useNullAsDefault: true,
   //   connection: {
-  //     filename: './data/medcabinet.db3'
+  //     filename: './data/cannabis_strains2.db'
   //   },
   //   migrations: {
   //     directory: './data/migrations'
@@ -39,6 +20,25 @@ module.exports = {
   //     }
   //   }
   // },
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/medcabinet.db3'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys=ON', done);
+      }
+    }
+  },
   
 
 
