7e7be8181bffcd679c55e9bf9214522a9497d850
vladmog <vladmog@gmail.com>
2019-08-30 02:26:17 -0700

    running tests

--- saved/saved-model.js
+++ saved/saved-model.js
@@ -53,38 +53,3 @@ function add(post){
         })
     
 }
-
-
-
-// function add(post){
-//     // console.log(post)
-//     return db('reviews').insert(post)
-//         .then(([id]) => {
-//             return findBy("id", id)
-//                 .then((review) => {
-//                     return db('reviews')
-//                         .select('strain_id')
-//                         .where({ user_id: review.user_id})
-//                         .then((strain_ids_objs) => {
-//                             let first = true
-//                             strain_ids_objs.map((strain_id_obj) => {
-//                                 let strain_id = strain_id_obj.strain_id;
-//                                 return db('strains')
-//                                     .where({id: strain_id})
-//                                     .first()
-//                                     .then((strain) => {
-//                                         if(first){
-//                                             console.log(strain)
-//                                             first = false
-//                                         }
-//                                         savedStrains.push(strain)
-//                                     })
-//                             })
-//                         })
-//                         .then(() => {
-//                             console.log("savedStrains", savedStrains)
-//                             return savedStrains
-//                         })
-//                 })
-//         })
-// }
