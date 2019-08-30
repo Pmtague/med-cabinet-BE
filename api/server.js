6536720a0e46e66d2f6520206fe9cba8d6363291
vladmog <vladmog@gmail.com>
2019-08-30 13:10:01 -0700

    added cors headers

--- api/server.js
+++ api/server.js
@@ -3,6 +3,13 @@ const apiRouter = require('./api-router');
 
 const server = express();
 
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin); //req.headers.origin // '*'
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Set-Cookie");
+    res.header('Access-Control-Allow-Credentials', true)
+    next();
+});
+
 server.use(express.json());
 server.use('/api', apiRouter);
 
