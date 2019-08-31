890af489fafefcc46d7503fa1fc1f49b8191661a
vladmog <vladmog@gmail.com>
2019-08-31 11:59:36 -0700

    Installed CORS

--- api/server.js
+++ api/server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const apiRouter = require('./api-router');
+const cors = require('cors')
 
 const server = express();
 
-server.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", req.headers.origin); //req.headers.origin // '*'
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Set-Cookie");
-    res.header('Access-Control-Allow-Credentials', true)
-    next();
-});
+server.use(cors())
+
+// server.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", req.headers.origin); //req.headers.origin // '*'
+//     // res.header("Access-Control-Allow-Origin", '*'); //req.headers.origin // '*'
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Set-Cookie");
+//     res.header('Access-Control-Allow-Credentials', true)
+//     next();
+// });
 
 server.use(express.json());
 server.use('/api', apiRouter);
